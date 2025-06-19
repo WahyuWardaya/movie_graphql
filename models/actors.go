de114b370b0a141d8aabffb5b7c943a8735cb14a
DegusPutra <igedeagusperdana03@gmail.com>
2025-06-19 19:53:09 +0800

    update model actors

---
+++ models/actors.go
@@ -0,0 +1,12 @@
+package models
+
+type Actors struct {
+	ID     uint     gorm:"column:id_actors;primary_key" json:"id"
+	Name   string   gorm:"column:name_actors" json:"name_actors"
+	Photo  string   gorm:"column:photo" json:"photo"
+	Movies []Movies gorm:"many2many:movie_actors;foreignKey:ID;joinForeignKey:id_actors;References:ID;joinReferences:id_movies" json:"-"
+}
+
+func (Actors) TableName() string {
+	return "actors"
+}
